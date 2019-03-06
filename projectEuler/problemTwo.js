eeca154f0723945c7668a0b9d6171c74c7264d27
Alex Puga <alpuga8@gmail.com>
2019-03-06 14:12:14 -0500

    problem 2 solution

--- projectEuler/problemTwo.js
+++ projectEuler/problemTwo.js
@@ -3,11 +3,13 @@
 /* Sum of the even numbers in nth terms*/
 
 function fiboEvenSum(n) {
+  //declare variables
   let sum = 0;
   let current = 1;
   let previous = 1;
   let newArr = [];
 
+  //run a loop to push fib number at each index to new array
   for (var i = 0; i < n; i++) {
     if (current == 1) {
       newArr.push(current);
@@ -17,13 +19,15 @@ function fiboEvenSum(n) {
     newArr.push(current);
   }
 
+  //loop through the new array of fib numbers and sum the even ones
   for (var j = 0; j < newArr.length; j++) {
     if (newArr[j] % 2 == 0) {
       sum += newArr[j];
     }
   }
-  console.log(sum);
+
   return sum;
 }
 
+//test
 fiboEvenSum(0);
