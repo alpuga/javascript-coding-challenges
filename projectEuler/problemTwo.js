615d256b79a558f60f6cde352536cf1b1fd75cac
Alex Puga <alpuga8@gmail.com>
2019-03-05 21:09:20 -0500

    solution to problem 2

--- projectEuler/problemTwo.js
+++ projectEuler/problemTwo.js
@@ -1,9 +1,29 @@
 /*jshint esversion: 6 */
 
-/* Even Finonacci Numbers Sum */
+/* Sum of the even numbers in nth terms*/
 
 function fiboEvenSum(n) {
-  return true;
+  let sum = 0;
+  let current = 1;
+  let previous = 1;
+  let newArr = [];
+
+  for (var i = 0; i < n; i++) {
+    if (current == 1) {
+      newArr.push(current);
+    }
+    current += previous;
+    previous = current - previous;
+    newArr.push(current);
+  }
+
+  for (var j = 0; j < newArr.length; j++) {
+    if (newArr[j] % 2 == 0) {
+      sum += newArr[j];
+    }
+  }
+  console.log(sum);
+  return sum;
 }
 
-fiboEvenSum(10);
+fiboEvenSum(0);
