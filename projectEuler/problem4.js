cf0f6cfb743b7885ca81fd0889ba1fbd84aff9e4
Alex Puga <alpuga8@gmail.com>
2019-03-08 13:48:20 -0500

    solution to problem 4

--- projectEuler/problem4.js
+++ projectEuler/problem4.js
@@ -9,7 +9,30 @@ Find the largest palindrome made from the product
 of two n-digit numbers. */
 
 function largestPalindromeProduct(n) {
-  return true;
+  // Using the Unary operator, makes sure max is a number.
+  // Use the reduce function to add 9 to an empty string as many times as n.
+  let max = +[...Array(n)].reduce((a, c) => (a += 9), "");
+
+  // Calculate min using max / 10.
+  let min = (max + 1) / 10;
+
+  //Store product of numbers between the limits in an array.
+  let products = [];
+
+  // start to loops to multiply numbers between limits.
+  for (let i = max; i >= min; i--) {
+    for (let j = max; j >= min; j--) {
+      let product = i * j;
+      // reverse the product to check palindrome
+      let productReverse = [...String(product)].reverse().join("");
+      // If
+      if (product == productReverse) {
+        products.push(product);
+      }
+    }
+  }
+
+  return Math.max(...products);
 }
-console.log("he");
-largestPalindromeProduct(3);
+
+largestPalindromeProduct(2);
